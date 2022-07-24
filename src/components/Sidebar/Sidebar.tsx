bb00a10b312e7168d146fa8e5798dac3227a67ce
C0D3RED <buxdma@gmail.com>
2022-07-24 23:05:13 +0300

    add remove button

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,27 +1,58 @@
 import React from 'react';
 import {observer} from "mobx-react";
-import {Link, useLocation} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
+import modalStore from '../../stores/modalStore'
 import sidebarStore, {ISidebarItem} from "../../stores/sidebarStore";
 import "./Sidebar.css"
+import { AiOutlineClose } from 'react-icons/ai'
+import todoListStore from '../../stores/todoListStore'
+
 
 const Sidebar = () => {
-    const location = useLocation().pathname;
-    return (
-        <div className="sidebar">
-            {sidebarStore.getItems().map((item: ISidebarItem) => (
-                <Link key={item.path} to={item.path}
-                      className={location === item.path
-                          ? "sidebar-item selected"
-                          : "sidebar-item" }>
-                    {item.title}
-                </Link>
+   const location = useLocation().pathname;
+   const navigate = useNavigate();
 
-            ))}
-            <div className="sidebar-item sidebar-add">
-                <div>Create new list</div>
-            </div>
-        </div>
-    );
+   const handleModalSubmit = (text: string) => {
+      const path = sidebarStore.addItem(text.trim());
+      navigate(path);
+   }
+
+   const handleRemove = (path: string) => {
+      navigate("/");
+      sidebarStore.removeItem(path);
+      todoListStore.removeTodos(path);
+   }
+
+   return (
+      <div className="sidebar">
+         {sidebarStore.getItems().map((item: ISidebarItem) => (
+            <Link key={item.path} to={item.path}
+                  className={location === item.path
+                     ? "sidebar-item selected"
+                     : "sidebar-item" }>
+               <div className="sidebar-item-text">
+                  {item.title}
+               </div>
+               {item.path !== "/"
+                  ? <AiOutlineClose className="sidebar-item-remove"
+                                    onClick={e => {
+                                       e.preventDefault();
+                                       handleRemove(item.path);
+                                    }}
+                  />
+                  : null
+               }
+            </Link>
+         ))}
+         <div className="sidebar-item sidebar-add"
+              onClick={() =>
+                 modalStore.show(
+                    "Add a task list",
+                    handleModalSubmit)}>
+            Create new list
+         </div>
+      </div>
+   );
 };
 
 export default observer(Sidebar);
