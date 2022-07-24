746e9343256301ca565b5349c6f2d741312ed8a7
C0D3RED <buxdma@gmail.com>
2022-07-24 23:04:06 +0300

    replace todo form with input form

---
+++ src/components/InputForm/InputForm.tsx
@@ -0,0 +1,33 @@
+import React, { FC, useState } from 'react'
+import {IoIosAdd} from "react-icons/io"
+import "./InputForm.css"
+
+
+interface IInputFormProps {
+   handleSubmit: (text: string) => void
+}
+
+const InputForm: FC<IInputFormProps> = ({handleSubmit}) => {
+   const [input, setInput] = useState("");
+
+   const handleClick = () => {
+      handleSubmit(input);
+      setInput("");
+   }
+
+   return (
+      <div className="input-form">
+         <input  autoFocus type="text"
+                 name="text" className="form-input"
+                 onChange={e => setInput(e.target.value)}
+                 value={input}
+                 onKeyDownCapture={e => {
+                    if (e.key === "Enter") handleClick();
+                 }}
+         />
+         <IoIosAdd className="form-button" onClick={handleClick} />
+      </div>
+   );
+};
+
+export default InputForm;
